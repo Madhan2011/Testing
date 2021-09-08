ecc4d8dd4b913679858cc5d370f24ead7e66e138
Madhan2011 <madhanmiller70@gmail.com>
2021-09-08 15:42:14 +0530

    changes made

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ class Test:
         self.a = int(input("a : "))
         self.b = int(input("b : "))
 
-    def name(self):
+    def addition(self):
         print(self.a + self.b)
 
+    def subtraction(self):
+        print(self.a - self.b)
 
-if __name__ == '__main__':
-    obj = Test()
-    obj.name()
+    def multiplication(self):
+        print(self.a * self.b)
+
+    def division(self):
+        print(self.a / self.b)
+
+    if __name__ == '__main__':
+        obj = Test()
+        obj.addition()
+        obj.subtraction()
+        obj.multiplication()
+        obj.division()
