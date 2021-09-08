d8379068cf7d31f06aa42ac77a212a3fff856751
Madhan2011 <madhanmiller70@gmail.com>
2021-09-08 15:13:37 +0530

    Added arithmetic functions

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ class Test:
         self.a = int(input("a : "))
         self.b = int(input("b : "))
 
-    def name(self):
+    def addition(self):
         print(self.a + self.b)
+
+    def subtraction(self):
+        print(self.a - self.b)
+
+    def multiplication(self):
+        print(self.a * self.b)
+
+    def division(self):
+        print(self.a / self.b)
 
 
 if __name__ == '__main__':
     obj = Test()
-    obj.name()
+    obj.addition()
+    obj.subtraction()
+    obj.multiplication()
+    obj.division()
 
 
 
