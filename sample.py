c3e4ec6e4d15be7950b581a62a3b66704e8f0279
Madhan2011 <madhanmiller70@gmail.com>
2021-09-13 18:58:58 +0530

    Pymongo updated

--- sample.py
+++ sample.py
@@ -7,7 +7,7 @@ cluster = MongoClient("mongodb+srv://madhan:1234@cluster0.a0liu.mongodb.net/data
                       ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 
 db = cluster["data"]
-collection = db["user_info1"]
+collection = db["user_info"]
 _date_ = datetime.datetime.now()
 
 
@@ -15,5 +15,11 @@ data = [{"_id": 21, "date": _date_, "name": "Anandh", "email": "anandh78@gmail.c
         {"_id": 22, "date": _date_, "name": "Praveen", "email": "praveenpappu3130@gmail.com.com", "phone": "9151181245"},
         {"_id": 23, "date": _date_, "name": "Udhay", "email": "udhayakumar@gmail.com", "phone": "9725511580"}]
 
-collection.insert_many(data)
+# collection.insert_many(data)
+collection.delete_one({"_id": 4})
+collection.insert_one({"_id": 4, "date": _date_, "name": "Sundar", "email": "sundar78@gmail.com.com", "phone": "7889201018"})
+res = collection.find({})
+
+for x in res:
+    print(x)
 
