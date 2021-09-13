1acf8848d1f3fbf2c021c8645227f84a812e0830
Madhan2011 <madhanmiller70@gmail.com>
2021-09-13 15:13:52 +0530

    Pymongo added

--- sample.py
+++ sample.py
@@ -1,12 +1,19 @@
-def main(b):
-    a = " Welcome, "
-    print(a + b)
+import pymongo
+from pymongo import MongoClient
+import ssl
+import datetime
+
+cluster = MongoClient("mongodb+srv://madhan:1234@cluster0.a0liu.mongodb.net/database?retryWrites=true&w=majority",
+                      ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+
+db = cluster["data"]
+collection = db["user_info1"]
+_date_ = datetime.datetime.now()
 
 
-main("Madhan")
+data = [{"_id": 21, "date": _date_, "name": "Anandh", "email": "anandh78@gmail.com.com", "phone": "7889201118"},
+        {"_id": 22, "date": _date_, "name": "Praveen", "email": "praveenpappu3130@gmail.com.com", "phone": "9151181245"},
+        {"_id": 23, "date": _date_, "name": "Udhay", "email": "udhayakumar@gmail.com", "phone": "9725511580"}]
 
-
-
-
-
+collection.insert_many(data)
 
